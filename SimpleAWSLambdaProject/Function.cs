6f7201b033e51470ada6f37b892945c4792833bc
Vidhu Popli <vidhu.popli@woodmac.com>
2025-03-09 17:41:40 +0530

    Add comment for passing string to Lambda

--- SimpleAWSLambdaProject/Function.cs
+++ SimpleAWSLambdaProject/Function.cs
@@ -6,7 +6,7 @@ namespace SimpleAWSLambdaProject;
 
 public class Function
 {
-    
+    // Run Mock Lambda Test Tool profile and pass a string like this "Vidhu"
     public string FunctionHandler(string input, ILambdaContext context)
     {
         return input.ToUpper();
